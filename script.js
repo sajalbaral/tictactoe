37932356768a58cbcf4bd2b59df65d126f1c5421
Sajal Baral <sajalsbaral@gmail.com>
2025-06-09 16:44:08 -0400

    added update board function that checks for valid/invalid moves

--- script.js
+++ script.js
@@ -7,10 +7,24 @@ function gameBoard() {
     }
   }
 
+  const updateBoard = (index, marker) => {
+    if (index >= 0 && index < board.length) {
+      if (board[index] === "") {
+        board[index] = marker;
+        return "move accepted";
+      } else {
+        return "space is already taken";
+      }
+    } else {
+      return "invalid move";
+    }
+  };
+
   //function that calls for simple returns
   const getBoard = () => board;
 
   return {
     getBoard,
+    updateBoard,
   };
 }
