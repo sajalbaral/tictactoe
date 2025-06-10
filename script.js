4eebb842df4ee8ac916a7b5f8363b294c494e65a
Sajal Baral <sajalsbaral@gmail.com>
2025-06-10 15:52:14 -0400

    added a gameController function that plays rounds, switches player, and returns currentplayer

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 function gameBoard() {
-  let board = [];
-  // loop that creates an array of 9 strings
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      board.push("");
-    }
-  }
+  let board = Array(9).fill("");
 
+  //updates the board based on player move
   const updateBoard = (index, marker) => {
     if (index >= 0 && index < board.length) {
       if (board[index] === "") {
@@ -20,11 +15,59 @@ function gameBoard() {
     }
   };
 
+  //resets the board with empty strings
+  const resetBoard = () => {
+    board = Array(9).fill("");
+  };
+
   //function that calls for simple returns
   const getBoard = () => board;
+  const reset = () => resetBoard();
 
   return {
     getBoard,
     updateBoard,
+    reset,
+  };
+}
+
+//used to create the two players
+function createPlayer(name, marker) {
+  return {
+    getName: () => name,
+    getMarker: () => marker,
+    resetBoard,
+  };
+}
+
+//controls the logic of how the game is played
+function gameController(playerOne = "Player One", playerTwo = "Player Two") {
+  const player1 = createPlayer(playerOne, "X");
+  const player2 = createPlayer(playerTwo, "O");
+  let currentPlayer = player1;
+
+  const board = gameBoard();
+
+  //places, updates, and switches players based on the move made
+  const playRound = (index) => {
+    const marker = currentPlayer.getMarker();
+    const result = board.updateBoard(index, marker);
+
+    if (result === "move accepted") {
+      switchPlayer();
+    }
+  };
+
+  //logic for switching players
+  const switchPlayer = () => {
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+  };
+
+  const getCurrPlayer = () => currentPlayer;
+
+  return {
+    getCurrPlayer,
+    playRound,
+    getBoard: board.getBoard,
   };
 }
